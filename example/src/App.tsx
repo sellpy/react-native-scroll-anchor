94cd57ecac862fc5996e8fd420b90972d8985b8d
Olle Thunberg <olle.thunberg03@gmail.com>
2024-04-09 23:14:08 +0200

    Use AnchorScrollView in example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,63 +6,66 @@ import {
   Dimensions,
   Keyboard,
   SafeAreaView,
-  ScrollView,
   StyleSheet,
   Text,
   TextInput,
   View,
 } from 'react-native';
-import { Anchor, ScrollAnchorProvider, useScrollAnchor } from '../../src';
+import { Anchor, AnchorScrollView, useScrollAnchorContext } from '../../src';
 
 export default function App() {
   const width = Dimensions.get('window').width;
-  const scrollRef = React.useRef<ScrollView>(null);
-  const methods = useScrollAnchor(scrollRef);
-
-  const [inputValue, setInputValue] = React.useState('0');
 
   return (
     <SafeAreaView style={styles.container}>
+      <AnchorScrollView
+        onAnchorReachedY={(name) => console.log('Reached anchor: ' + name)}
+        contentContainerStyle={{ width: width, paddingHorizontal: 20 }}
+      >
+        <ScrollToButtons />
+        {new Array(100).fill(0).map((_, i) => (
+          <Anchor key={i} name={'anchor' + i}>
+            <View
+              style={{
+                backgroundColor: i % 2 ? 'red' : 'blue',
+              }}
+            >
+              <Text style={{ fontSize: 20, paddingVertical: 8 }}>
+                Element: {i}
+              </Text>
+            </View>
+          </Anchor>
+        ))}
+      </AnchorScrollView>
+    </SafeAreaView>
+  );
+}
+
+const ScrollToButtons = () => {
+  const [inputValue, setInputValue] = React.useState('0');
+
+  const { scrollTo } = useScrollAnchorContext();
+
+  return (
+    <>
       <TextInput
         style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 100 }}
         value={inputValue}
         onChangeText={setInputValue}
         inputMode="numeric"
-        onSubmitEditing={() => methods.scrollTo('anchor' + inputValue)}
+        onSubmitEditing={() => scrollTo('anchor' + inputValue)}
       />
       <Button
         title={'Scroll to element ' + inputValue}
         onPress={() => {
-          methods.scrollTo('anchor' + inputValue);
+          scrollTo('anchor' + inputValue);
           Keyboard.dismiss();
         }}
         color={'blue'}
       />
-      <ScrollView
-        onScroll={methods.onScroll}
-        scrollEventThrottle={32}
-        contentContainerStyle={{ width: width, paddingHorizontal: 20 }}
-        ref={scrollRef}
-      >
-        <ScrollAnchorProvider {...methods}>
-          {new Array(100).fill(0).map((_, i) => (
-            <Anchor key={i} name={'anchor' + i}>
-              <View
-                style={{
-                  backgroundColor: i % 2 ? 'red' : 'blue',
-                }}
-              >
-                <Text style={{ fontSize: 20, paddingVertical: 8 }}>
-                  Element: {i}
-                </Text>
-              </View>
-            </Anchor>
-          ))}
-        </ScrollAnchorProvider>
-      </ScrollView>
-    </SafeAreaView>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {

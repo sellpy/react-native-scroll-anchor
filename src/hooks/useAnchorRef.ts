778e3114c891d625983e1844519597d081935c05
Olle Thunberg <olle.thunberg03@gmail.com>
2024-04-10 07:40:19 +0200

    Rename from key to name (internally) since it causes name collision

--- src/hooks/useAnchorRef.ts
+++ src/hooks/useAnchorRef.ts
@@ -2,15 +2,15 @@ import { useRef, useEffect } from 'react';
 import { useScrollAnchorContext } from '../Context';
 import type { NativeComponent } from './useScrollAnchor';
 
-export const useAnchorRef = (key: string) => {
+export const useAnchorRef = (name: string) => {
   const { register, unregister } = useScrollAnchorContext();
   const ref = useRef<NativeComponent>(null);
 
   useEffect(() => {
-    if (ref && ref.current) register(key, ref);
-    return () => unregister(key);
+    if (ref && ref.current) register(name, ref);
+    return () => unregister(name);
     // eslint-disable-next-line react-hooks/exhaustive-deps -- only run on mount
-  }, [key, ref.current]);
+  }, [name, ref.current]);
 
   return ref;
 };
